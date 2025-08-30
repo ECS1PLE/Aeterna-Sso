3a2a8dda55a00b3b0f5f33d899ad89a838886cd3
Artyom <artyom.vavilov78@gmail.com>
2025-08-31 01:32:12 +0300

    Add Logo

--- src/components/Blocks/MainBlock.tsx
+++ src/components/Blocks/MainBlock.tsx
@@ -1,3 +1,5 @@
+import Logo from "../Illustrations/Logo";
+
 interface MainBlockProps {
     children?: React.ReactNode;
     className?: string;
@@ -7,10 +9,11 @@ const MainBlock: React.FC<MainBlockProps> = ({ children, className }) => {
     return (
         <div className="flex min-h-screen">
             <div
-                className={`flex w-[409px] h-[519px] max-w-[409px] flex-col items-start p-11 gap-[43px] 
+                className={`flex h-auto max-w-[409px] flex-col items-start p-11 gap-[43px] 
                 bg-gradient-to-b from-[#1F1F1F] to-[#0A0A0A] border-[0.5px] border-neutral-600 
                 rounded-3xl ml-[248px] mt-auto mb-auto ${className}`}
-            >
+            >   
+                <Logo />
                 {children}
             </div>
         </div>
