036be9d3cd61fbff3a4459541f81f73bba7c97e9
ECS1PLE <119251348+ECS1PLE@users.noreply.github.com>
2025-09-01 23:31:51 +0300

    Add return button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geologica, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Background from "./Background";
 import Block from "./Block";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const geologica = Geologica({
   variable: "--font-geologica",
@@ -37,6 +39,29 @@ export default function RootLayout({
       <body
         className={`${geologica.variable} ${geistMono.variable} antialiased bg-black h-[100vh]`}
       >
+        <Link href="/">
+          <div
+            className="flex gap-[18px] absolute z-2 top-[55px] left-[52px] w-auto"
+            style={{ color: "var(--Typography-secondary, #FFFFFFB2)" }}
+          >
+            <ArrowLeft className="stroke-current" />
+            <p
+              className="
+              font-[Geologica]        
+              font-normal              
+              text-[20.45px]           
+              leading-[100%]          
+              tracking-[-0.05em]      
+              align-middle  
+              mt-auto
+              mb-auto
+            "
+            >
+              Вернуться назад
+            </p>
+          </div>
+        </Link>
+
         <Background />
         <Block />
         {children}
